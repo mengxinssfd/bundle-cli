91cdfa0afc110f98c8e1dc185852436811d3da3e
DYH <xinzon32177@163.com>
2021-07-08 12:07:16 +0800

    test:

--- test/index.js
+++ test/index.js
@@ -2,9 +2,13 @@ import * as utils from "./utils";
 
 console.log(utils.inRange(100, [20]));
 
+console.log(utils.isArray({}));
+console.log(utils.isArray(utils.isNumber([])));
 const a = 10;
 let b = 1;
 b += a;
 console.log(b);
 const rand = Math.random();
 console.log(rand);
+
+export * from "./utils";
--- test/utils.js
+++ test/utils.js
@@ -1,26 +1,33 @@
-export function forEach(arr, callbackFn, elseCB) {
-  // 不能直接把arr.length放进循环，否则在循环里新增的话length会变长,原生的不会变长
-  const len = arr.length || 0;
-  // if (!isArrayLike(arr)) throw new TypeError();
-  for (let i = 0; i < len; i++) {
-    if (callbackFn(arr[i], i, arr) === false)
-      return false;
-  }
-  elseCB && elseCB();
-  return true;
-}
-
-export async function forEachAsync(cbAsync, thisArg) {
-  const arr = thisArg || this;
-  // 不能直接把arr.length放进循环，否则在循环里新增的话length会变长,原生的不会变长
-  const len = arr.length;
-  // if (!isArrayLike(arr)) throw new TypeError();
-  for (let i = 0; i < len; i++) {
-    const v = await cbAsync(arr[i], i, arr);
-    if (v === false)
-      break;
-  }
-}
 export function inRange(value, [min = -Infinity, max = Infinity]) {
   return min <= value && value <= max;
+}
+
+export function typeOf(target) {
+  const tp = typeof target;
+  if (tp !== "object")
+    return tp;
+  return Object.prototype.toString.call(target).slice(8, -1).toLowerCase();
+}
+export function isObject(target) {
+  return typeOf(target) === "object";
+}
+export function isObjectLike(value) {
+  const type = typeof value;
+  return value != null && (type === "object" || type === "function");
+}
+export const isBroadlyObj = isObjectLike;
+export function isArray(target) {
+  return typeOf(target) === "array";
+}
+export function isString(target) {
+  return typeOf(target) === "string";
+}
+export function isNumber(target) {
+  return typeOf(target) === "number";
+}
+export function isFunction(target) {
+  return typeOf(target) === "function";
+}
+export function isBoolean(target) {
+  return typeOf(target) === "boolean";
 }
