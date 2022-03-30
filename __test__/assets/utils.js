05247b506d260d6aae04bb335da835ca8ab11c7d
dyh_a <xinzon32177@163.com>
2022-03-31 00:48:53 +0800

    完善测试用例

--- __test__/assets/utils.js
+++ __test__/assets/utils.js
@@ -4,30 +4,31 @@ export function inRange(value, [min = -Infinity, max = Infinity]) {
 // comments 1111111111111111
 export function typeOf(target) {
   const tp = typeof target;
-  if (tp !== "object")
-    return tp;
+  if (tp !== 'object') return tp;
   return Object.prototype.toString.call(target).slice(8, -1).toLowerCase();
 }
 export function isObject(target) {
-  return typeOf(target) === "object";
+  return typeOf(target) === 'object';
 }
 export function isObjectLike(value) {
   const type = typeof value;
-  return value != null && (type === "object" || type === "function");
+  return value != null && (type === 'object' || type === 'function');
 }
 export const isBroadlyObj = isObjectLike;
 export function isArray(target) {
-  return typeOf(target) === "array";
+  return typeOf(target) === 'array';
 }
 export function isString(target) {
-  return typeOf(target) === "string";
+  return typeOf(target) === 'string';
 }
 export function isNumber(target) {
-  return typeOf(target) === "number";
+  return typeOf(target) === 'number';
 }
 export function isFunction(target) {
-  return typeOf(target) === "function";
+  return typeOf(target) === 'function';
 }
 export function isBoolean(target) {
-  return typeOf(target) === "boolean";
+  return typeOf(target) === 'boolean';
 }
+
+export const arrowFn = (t) => typeOf(t);
